68320e39d7fdf20ab3f7dd7ff290bf42f51d6030
v25 <>
2020-05-20 23:46:09 +0100

    fixed syntax to make clearer

--- app.py
+++ app.py
@@ -43,14 +43,17 @@ def search_result():
     return render_template('index.html', headers = headers, objects = objects, display_search=True)
 
 
-original_items = [{'item 1': "Banana", 'item 2': "Car"}, {'item 1': "Apple", 'item 2': "Bike"}]
-items = [ list(v for _,v in x.items()) for x in original_items]
+import collections
+def sort_dict(d):    
+    return collections.OrderedDict(sorted(d.items()))
 
-
-headers = ['item 1','item 2']
-
-
-
+# https://stackoverflow.com/a/61645014
 @app.route('/all')
 def all():
-  return render_template('index.html', headers=headers, objects=items)
+    original_items = [{'item 1': "Banana", 'item 2': "Car"}, {'item 1': "Apple", 'item 2': "Bike"}]
+    ods = [sort_dict(d) for d in original_items]
+    table_items = [ list(v for _,v in od.items()) for od in ods]
+    headers = [k for k,_ in ods[0].items()]
+    return render_template('index.html', headers=headers, objects=table_items)
+
+
